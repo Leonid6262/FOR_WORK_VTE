3213916b51478a6e0b5629494ebd7217813813e0
Leonid6262 <leonid.ovchinnikov.62@gmail.com>
2026-01-23 12:56:48 +0200

    Корректировки по результатам проверок

--- SRC/PULSES/SIFU.cpp
+++ SRC/PULSES/SIFU.cpp
@@ -5,104 +5,101 @@
 const unsigned char CSIFU::pulses[] = {0x00, 0x21, 0x03, 0x06, 0x0C, 0x18, 0x30};       // Индекс 0 не используется
 const unsigned char CSIFU::pulse_w_one[] = {0x00, 0x21, 0x00, 0x06, 0x00, 0x18, 0x00};  // Индекс 0 не используется
 const signed short CSIFU::offsets[] = {
-    0,
-    SIFUConst::_60gr,    // Диапазон 0...60        (sync "видит" 1-й: ->2-3-4-5-6-sync-1->2-3-4...)
-    SIFUConst::_120gr,   // Диапазон -60...0       (sync "видит" 2-й: ->3-4-5-6-1-sync-2->3-4-5...)
-    SIFUConst::_180gr,   // Диапазон -120...-60    (sync "видит" 3-й: ->4-5-6-1-2-sync-3->4-5-6...) - чисто теоретически
-    -SIFUConst::_120gr,  // Диапазон 180...240     (sync "видит" 4-й: ->5-6-1-2-3-sync-4->5-6-1...) - чисто теоретически
-    -SIFUConst::_60gr,   // Диапазон 120...180     (sync "видит" 5-й: ->6-1-2-3-4-sync-5->6-1-2...)
-    SIFUConst::_0gr      // Диапазон 60...120      (sync "видит" 6-й: ->1-2-3-4-5-sync-6->1-2-3...)
+  0,
+  SIFUConst::_60gr,    // Диапазон 0...60        (sync "видит" 1-й: ->2-3-4-5-6-sync-1->2-3-4...)
+  SIFUConst::_120gr,   // Диапазон -60...0       (sync "видит" 2-й: ->3-4-5-6-1-sync-2->3-4-5...)
+  SIFUConst::_180gr,   // Диапазон -120...-60    (sync "видит" 3-й: ->4-5-6-1-2-sync-3->4-5-6...) - чисто теоретически
+  -SIFUConst::_120gr,  // Диапазон 180...240     (sync "видит" 4-й: ->5-6-1-2-3-sync-4->5-6-1...) - чисто теоретически
+  -SIFUConst::_60gr,   // Диапазон 120...180     (sync "видит" 5-й: ->6-1-2-3-4-sync-5->6-1-2...)
+  SIFUConst::_0gr      // Диапазон 60...120      (sync "видит" 6-й: ->1-2-3-4-5-sync-6->1-2-3...)
 };  // Индекс 0 не используется
 
-CSIFU::CSIFU(CPULSCALC& rPulsCalc, CRegManager& rReg_manager, CFaultCtrlP& rFault_p, CEEPSettings& rSettings) : 
-  rPulsCalc(rPulsCalc), 
-  rReg_manager(rReg_manager), 
-  rFault_p(rFault_p),
-  rSettings(rSettings){}
+CSIFU::CSIFU(CPULSCALC& rPulsCalc, CRegManager& rReg_manager, CFaultCtrlP& rFault_p, CEEPSettings& rSettings) 
+: rPulsCalc(rPulsCalc), rReg_manager(rReg_manager), rFault_p(rFault_p), rSettings(rSettings){}
 
-  void CSIFU::rising_puls() {
-    N_Pulse = (N_Pulse % s_const.N_PULSES) + 1;
+void CSIFU::rising_puls() {
+  N_Pulse = (N_Pulse % s_const.N_PULSES) + 1;
+  
+  // Старт ИУ рабочего моста
+  if (main_bridge) {
+    LPC_GPIO3->CLR = pulses[(((N_Pulse - 1) + v_sync.d_power_shift) % s_const.N_PULSES) + 1] << FIRS_PULS_PORT;
     
-    // Старт ИУ рабочего моста
-    if (main_bridge) {
-      LPC_GPIO3->CLR = pulses[(((N_Pulse - 1) + v_sync.d_power_shift) % s_const.N_PULSES) + 1] << FIRS_PULS_PORT;
-      
-      LPC_SC->PCONP |= CLKPWR_PCONP_PCPWM0; 
-      
-      LPC_PWM0->PR = PWM_div_0 - 1;
-      LPC_PWM0->MCR = MR0R; // Reset TC on MR0
-      LPC_PWM0->MR0 = PWM_WIDTH * 2;
-      LPC_PWM0->MR1 = PWM_WIDTH;
-      LPC_PWM0->LER = LER_012; 
-      LPC_PWM0->PCR |= PCR_PWMENA1;
-      // Важно
-      LPC_PWM0->TCR = COUNTER_RESET; // Обнулили TC и PR
-      LPC_PWM0->TC  = LPC_PWM0->MR0; // Вручную ставим счетчик в значение финиша
-      // ----
-      LPC_IOCON->P1_2 = IOCON_P_PWM; 
-      LPC_PWM0->TCR = COUNTER_START; // Запускаем      
-    }
-    // Старт ИУ  форсировочного моста
-    if (forcing_bridge) {
-      LPC_GPIO3->CLR = pulses[(((N_Pulse - 1) + v_sync.d_power_shift) % s_const.N_PULSES) + 1] << FIRS_PULS_PORT;
-      
-      LPC_SC->PCONP |= CLKPWR_PCONP_PCPWM0; 
-      
-      LPC_PWM0->PR = PWM_div_0 - 1;
-      LPC_PWM0->MCR = MR0R; // Reset TC on MR0
-      LPC_PWM0->MR0 = PWM_WIDTH * 2;
-      LPC_PWM0->MR2 = PWM_WIDTH;
-      LPC_PWM0->LER = LER_012; 
-      LPC_PWM0->PCR |= PCR_PWMENA2;
-      // Важно
-      LPC_PWM0->TCR = COUNTER_RESET; // Обнулили TC и PR
-      LPC_PWM0->TC  = LPC_PWM0->MR0; // Вручную ставим счетчик в значение финиша
-      // ----
-      LPC_IOCON->P1_3 = IOCON_P_PWM; 
-      LPC_PWM0->TCR = COUNTER_START; // Запускаем
-    }
+    LPC_SC->PCONP |= CLKPWR_PCONP_PCPWM0; 
     
-    control_sync();  // Мониторинг события захвата синхроимпульса
-    
-  signed int cur_MR0 = static_cast<signed int>(LPC_TIM3->MR0);
-
-  switch (Operating_mode) {
-    case EOperating_mode::NO_SYNC: {
-      signed int res = static_cast<signed int>(LPC_TIM3->MR0) + s_const._60gr;
-      LPC_TIM3->MR0 = static_cast<unsigned int>(res);
-      curSyncStat = static_cast<unsigned char>(State::OFF);
-    }  // Старт следующего через 60 градусов
-    break;
-    case EOperating_mode::RESYNC: {
-      Operating_mode = EOperating_mode::NORMAL;  // Синхронизация с 1-го в Alpha_max
-      Alpha_setpoint = s_const.AMax;
-      Alpha_current = s_const.AMax;
-      // 1-2-3-4-sync-5->6-1-2-3-4-sync-5->6-1-2....
-      // Здесь и далее, кастования слагаемых приведены для наглядности,
-      // без магии циклической арифметики таймера по модулю 2^32
-      signed int res = static_cast<signed int>(v_sync.CURRENT_SYNC) + static_cast<signed int>(Alpha_current) +
-                       static_cast<signed int>(v_sync.cur_power_shift);
-      LPC_TIM3->MR0 = static_cast<unsigned int>(res);
-      N_Pulse = 6;
-      curSyncStat = static_cast<unsigned char>(State::OFF);
-    } break;
-    case EOperating_mode::PHASING:
-      // Ограничения величины сдвига
-      v_sync.task_power_shift = limits_val(&v_sync.task_power_shift, s_const.MinPshift, s_const.MaxPshift);
-      // Ограничения приращения сдвига
-      limits_dval(&v_sync.task_power_shift, &v_sync.cur_power_shift, s_const.dAlpha);
-      Alpha_setpoint = s_const._0gr;
-      LPC_TIM3->MR0 = timing_calc();  // Задание тайминга для следующего импульса
-      curSyncStat = static_cast<unsigned char>(State::ON);
-      break;
-    case EOperating_mode::NORMAL:
-      // Ограничения величины альфа
-      Alpha_setpoint = limits_val(&Alpha_setpoint, s_const.AMin, s_const.AMax);
-      LPC_TIM3->MR0 = timing_calc();  // Задание тайминга для следующего импульса
-      curSyncStat = static_cast<unsigned char>(State::ON);
-      break;
+    LPC_PWM0->PR = PWM_div_0 - 1;
+    LPC_PWM0->MCR = MR0R; // Reset TC on MR0
+    LPC_PWM0->MR0 = PWM_WIDTH * 2;
+    LPC_PWM0->MR1 = PWM_WIDTH;
+    LPC_PWM0->LER = LER_012; 
+    LPC_PWM0->PCR |= PCR_PWMENA1;
+    // Важно
+    LPC_PWM0->TCR = COUNTER_RESET; // Обнулили TC и PR
+    LPC_PWM0->TC  = LPC_PWM0->MR0; // Вручную ставим счетчик в значение финиша
+    // ----
+    LPC_IOCON->P1_2 = IOCON_P_PWM; 
+    LPC_PWM0->TCR = COUNTER_START; // Запускаем      
   }
-
+  // Старт ИУ  форсировочного моста
+  if (forcing_bridge) {
+    LPC_GPIO3->CLR = pulses[(((N_Pulse - 1) + v_sync.d_power_shift) % s_const.N_PULSES) + 1] << FIRS_PULS_PORT;
+    
+    LPC_SC->PCONP |= CLKPWR_PCONP_PCPWM0; 
+    
+    LPC_PWM0->PR = PWM_div_0 - 1;
+    LPC_PWM0->MCR = MR0R; // Reset TC on MR0
+    LPC_PWM0->MR0 = PWM_WIDTH * 2;
+    LPC_PWM0->MR2 = PWM_WIDTH;
+    LPC_PWM0->LER = LER_012; 
+    LPC_PWM0->PCR |= PCR_PWMENA2;
+    // Важно
+    LPC_PWM0->TCR = COUNTER_RESET; // Обнулили TC и PR
+    LPC_PWM0->TC  = LPC_PWM0->MR0; // Вручную ставим счетчик в значение финиша
+    // ----
+    LPC_IOCON->P1_3 = IOCON_P_PWM; 
+    LPC_PWM0->TCR = COUNTER_START; // Запускаем
+  }
+  
+  control_sync();  // Мониторинг события захвата синхроимпульса
+  
+  signed int cur_MR0 = static_cast<signed int>(LPC_TIM3->MR0);
+  
+  switch (Operating_mode) {
+  case EOperating_mode::NO_SYNC: {
+    signed int res = static_cast<signed int>(LPC_TIM3->MR0) + s_const._60gr;
+    LPC_TIM3->MR0 = static_cast<unsigned int>(res);
+    curSyncStat = static_cast<unsigned char>(State::OFF);
+  }  // Старт следующего через 60 градусов
+  break;
+  case EOperating_mode::RESYNC: {
+    Operating_mode = EOperating_mode::NORMAL;  // Синхронизация с 1-го в Alpha_max
+    Alpha_setpoint = s_const.AMax;
+    Alpha_current = s_const.AMax;
+    // 1-2-3-4-sync-5->6-1-2-3-4-sync-5->6-1-2....
+    // Здесь и далее, кастования слагаемых приведены для наглядности,
+    // без магии циклической арифметики таймера по модулю 2^32
+    signed int res = static_cast<signed int>(v_sync.CURRENT_SYNC) + static_cast<signed int>(Alpha_current) +
+      static_cast<signed int>(v_sync.cur_power_shift);
+    LPC_TIM3->MR0 = static_cast<unsigned int>(res);
+    N_Pulse = 6;
+    curSyncStat = static_cast<unsigned char>(State::OFF);
+  } break;
+  case EOperating_mode::PHASING:
+    // Ограничения величины сдвига
+    v_sync.task_power_shift = limits_val(&v_sync.task_power_shift, s_const.MinPshift, s_const.MaxPshift);
+    // Ограничения приращения сдвига
+    limits_dval(&v_sync.task_power_shift, &v_sync.cur_power_shift, s_const.dAlpha);
+    Alpha_setpoint = s_const._0gr;
+    LPC_TIM3->MR0 = timing_calc();  // Задание тайминга для следующего импульса
+    curSyncStat = static_cast<unsigned char>(State::ON);
+    break;
+  case EOperating_mode::NORMAL:
+    // Ограничения величины альфа
+    Alpha_setpoint = limits_val(&Alpha_setpoint, s_const.AMin, s_const.AMax);
+    LPC_TIM3->MR0 = timing_calc();  // Задание тайминга для следующего импульса
+    curSyncStat = static_cast<unsigned char>(State::ON);
+    break;
+  }
+  
   signed int res = cur_MR0 + SIFUConst::PULSE_WIDTH;
   //
   //
@@ -110,7 +107,7 @@ CSIFU::CSIFU(CPULSCALC& rPulsCalc, CRegManager& rReg_manager, CFaultCtrlP& rFaul
   //
   //
   LPC_TIM3->MR1 = static_cast<unsigned int>(res);  // Задание окончания текущего
-
+  
   if(phase_stop) {
     rReg_manager.setCurrent(State::OFF);
     rReg_manager.setQPower(State::OFF);
@@ -140,14 +137,14 @@ unsigned int CSIFU::timing_calc() {
     // Коррекция по синхронизации
     v_sync.SYNC_EVENT = false;
     signed int ret = static_cast<signed int>(v_sync.CURRENT_SYNC) + static_cast<signed int>(Alpha_current) +
-                     static_cast<signed int>(offsets[N_Pulse]) + static_cast<signed int>(v_sync.cur_power_shift);
+      static_cast<signed int>(offsets[N_Pulse]) + static_cast<signed int>(v_sync.cur_power_shift);
     return static_cast<unsigned int>(ret);
     // return (v_sync.CURRENT_SYNC + (A_Cur_tick + offsets[N_Pulse]) + v_sync.cur_power_shift);
   } else {
     // Продолжение последовательности
     signed int ret = static_cast<signed int>(LPC_TIM3->MR0) + static_cast<signed int>(s_const._60gr) +
-                     static_cast<signed int>(d_Alpha);
-
+      static_cast<signed int>(d_Alpha);
+    
     return static_cast<unsigned int>(ret);
     // return LPC_TIM3->MR0 + s_const._60gr + d_Alpha;
   }
@@ -177,16 +174,16 @@ void CSIFU::faling_puls() {
   LPC_GPIO1->CLR = 1UL << P1_2;
   LPC_IOCON->P1_3 = IOCON_P_PORT;  // P1_3 - Port
   LPC_GPIO1->CLR = 1UL << P1_3;
-
+  
   LPC_GPIO3->SET = OFF_PULSES;
-
+  
   LPC_SC->PCONP &= ~CLKPWR_PCONP_PCPWM0; // Выкл PWM
   
 }
 
 void CSIFU::control_sync() {
   v_sync.current_cr = LPC_TIM3->CR1;
-
+  
   if ((Operating_mode == EOperating_mode::NO_SYNC) && (v_sync.previous_cr != v_sync.current_cr)) {
     unsigned int dt = v_sync.current_cr - v_sync.previous_cr;
     v_sync.previous_cr = v_sync.current_cr;
@@ -202,7 +199,7 @@ void CSIFU::control_sync() {
     }
     return;
   }
-
+  
   if (Operating_mode == EOperating_mode::NORMAL || Operating_mode == EOperating_mode::PHASING) {
     if (v_sync.previous_cr != v_sync.current_cr) {
       unsigned int dt = v_sync.current_cr - v_sync.previous_cr;
@@ -276,7 +273,7 @@ unsigned char* CSIFU::getSyncStat() { return &curSyncStat; }
 void CSIFU::init_and_start(CProxyPointerVar& PPV) {
   forcing_bridge = false;
   main_bridge = false;
-
+  
   N_Pulse = 1;
   v_sync.d_power_shift = rSettings.getSettings().set_sifu.d_power_shift;
   v_sync.task_power_shift = rSettings.getSettings().set_sifu.power_shift;
@@ -289,20 +286,22 @@ void CSIFU::init_and_start(CProxyPointerVar& PPV) {
   Operating_mode = EOperating_mode::NO_SYNC;
   
   // Регистрация Alpha в реестре указателей
-  PPV.registerVar (NProxyVar::ProxyVarID::AlphaCur, &Alpha_current, 
-                   cd::Alpha, NProxyVar::Unit::Deg);
-
+  PPV.registerVar (NProxyVar::ProxyVarID::AlphaCur, 
+                   &Alpha_current, 
+                   cd::Alpha, 
+                   NProxyVar::Unit::Deg);
+  
   LPC_IOCON->P2_23 = IOCON_T3_CAP1;  // T3 CAP1
   LPC_TIM3->MCR = 0x00000000;        // Compare TIM3 с MR0 и MR1, с прерываниями (disabled)
   LPC_TIM3->IR = 0xFFFFFFFF;         // Очистка флагов прерываний
   LPC_TIM3->TCR |= TIM3_TCR_START;   // Старт таймера TIM3
-
+  
   LPC_TIM3->TC = 0;
   LPC_TIM3->MR0 = s_const._60gr;
   LPC_TIM3->MR1 = s_const._60gr + SIFUConst::PULSE_WIDTH;
   LPC_TIM3->CCR = TIM3_CAPTURE_RI;    // Захват T3 по фронту CAP1 без прерываний
   LPC_TIM3->MCR = TIM3_COMPARE_MR0;   // Compare TIM3 с MR0 - enabled
   LPC_TIM3->MCR |= TIM3_COMPARE_MR1;  // Compare TIM3 с MR1 - enabled
-
+  
   NVIC_EnableIRQ(TIMER3_IRQn);
 }
