61c1f0fa49a9e9e64a39d5ff894bae89e61e2cc5
L.Ovchinnikov <leonid.ovchinnikov.62@gmail.com>
2026-03-08 10:35:53 +0200

    Home

--- SRC/SERIAL/drv_spi.hpp
+++ SRC/SERIAL/drv_spi.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "LPC407x_8x_177x_8x.h"
-#include "spi_init.hpp"
-#include "system_LPC177x.h"
+//#include "LPC407x_8x_177x_8x.h"
+//#include "spi_init.hpp"
+//#include "system_LPC177x.h"
 
 class CSPI_driver {
 private:
@@ -11,7 +11,10 @@ public:
     CSPI_driver(LPC_SSP_TypeDef* SSP) : SSP(SSP) {}
 
     void writeByte(unsigned char data) { SSP->DR = data; }
-    void waitingBSY() { while (SSP->SR & SPI_Config::SR_BSY) {} }    
     unsigned char readByte() { return SSP->DR; }
+    bool statusBSY() { return (SSP->SR & SPI_Config::SR_BSY); } 
+    bool statusTNF() { return (SSP->SR & SPI_Config::SR_TNF); }
+    bool statusRNE() { return (SSP->SR & SPI_Config::SR_RNE); }
+
 };
 
